12ee36d2d67cdd3b1cfe26eec1f9ae66cf2b5c42
Jimena Ace <acevedo.romo.jimena.olivia@gmail.com>
2024-11-14 21:11:47 -0600

    Interfaz modificada. Tamano de fuente

--- main.py
+++ main.py
@@ -10,39 +10,36 @@ root.title("AES")
 valueProceso = tk.StringVar(value="-")
 valueModoOperacion = tk.StringVar(value="-")
 
-
 def inicio():
     limpiarVentana()
-    label = tk.Label(root, text="Práctica Cifrador por bloques (AES)", font=("Arial", 14, "bold"))
+    label = tk.Label(root, text="Práctica Cifrador por bloques (AES)", font=("Arial", 16, "bold"))
     label.pack()
 
-    pregunta = tk.Label(root, text="\n¿Qué quieres realizar?")
+    pregunta = tk.Label(root, text="\n¿Qué quieres realizar?", font=("Arial", 14))
     pregunta.pack()
 
-    
-
-    radioC = tk.Radiobutton(root, text="Cifrado", variable=valueProceso, value="cifrado")
+    radioC = tk.Radiobutton(root, text="Cifrado", variable=valueProceso, value="cifrado", font=("Arial", 14))
     radioC.pack()
 
-    radioD = tk.Radiobutton(root, text="Descifrado", variable=valueProceso, value="descifrado")
+    radioD = tk.Radiobutton(root, text="Descifrado", variable=valueProceso, value="descifrado", font=("Arial", 14))
     radioD.pack()
 
-    labelModoOperacion = tk.Label(root, text="\nElige el modo de operación:")
+    labelModoOperacion = tk.Label(root, text="\nElige el modo de operación:", font=("Arial", 14))
     labelModoOperacion.pack()
 
-    radioECB = tk.Radiobutton(root, text="ECB", variable=valueModoOperacion, value="ECB")
+    radioECB = tk.Radiobutton(root, text="ECB", variable=valueModoOperacion, value="ECB", font=("Arial", 14))
     radioECB.pack()
 
-    radioCBC = tk.Radiobutton(root, text="CBC", variable=valueModoOperacion, value="CBC")
+    radioCBC = tk.Radiobutton(root, text="CBC", variable=valueModoOperacion, value="CBC", font=("Arial", 14))
     radioCBC.pack()
 
-    radioCFB = tk.Radiobutton(root, text="CFB", variable=valueModoOperacion, value="CFB")
+    radioCFB = tk.Radiobutton(root, text="CFB", variable=valueModoOperacion, value="CFB", font=("Arial", 14))
     radioCFB.pack()
 
-    radioOFB = tk.Radiobutton(root, text="OFB", variable=valueModoOperacion, value="OFB")
+    radioOFB = tk.Radiobutton(root, text="OFB", variable=valueModoOperacion, value="OFB", font=("Arial", 14))
     radioOFB.pack()
 
-    buttonEnviar = tk.Button(root, text="Enviar", command=enviar)
+    buttonEnviar = tk.Button(root, text="Enviar", command=enviar, font=("Arial", 14))
     buttonEnviar.pack()
 
 def limpiarVentana():
@@ -51,8 +48,8 @@ def limpiarVentana():
 
 def buscarArchivo(labelArchivoEntrada,labelImg):
     global archivoBMP
-    root.filename = filedialog.askopenfilename(title="Buscar imagen", filetypes=([("archivos bmp", "*.bmp")]))
-    labelArchivoEntrada.config(text= "Ruta del archivo seleccionado:\n"+root.filename)
+    root.filename = filedialog.askopenfilename(title="Buscar imagen", filetypes=([("archivos bmp", "*.bmp")]), font=("Arial", 14))
+    labelArchivoEntrada.config(text= "Ruta del archivo seleccionado:\n"+root.filename, font=("Arial", 14))
     #labelArchivoEntrada.pack()
     img = Image.open(root.filename)
 
@@ -95,7 +92,6 @@ def cifradoOFB(ruta, llave, C0):
 def descifradoECB(ruta, llave, C0):
     messagebox.showinfo("info", "estas en modo ECB de descifrado")
     
-
 def descifradoCBC(ruta, llave, C0):
     messagebox.showinfo("info", "estas en modo CBC de descifrado")
 
@@ -104,8 +100,6 @@ def descifradoCFB(ruta, llave, C0):
 
 def descifradoOFB(ruta, llave, C0):
     messagebox.showinfo("info", "estas en modo OFB de descifrado")
-
-
 
 def AES(proceso, modoOperacion, ruta, llave, C0):
     limpiarVentana()
@@ -118,8 +112,7 @@ def AES(proceso, modoOperacion, ruta, llave, C0):
         if modoOperacion == "CFB":
             cifradoCFB(ruta, llave, C0)
         if modoOperacion == "OFB":
-            cifradoOFB(ruta, llave, C0)
-            
+            cifradoOFB(ruta, llave, C0)            
 
     if proceso == "descifrado":
         messagebox.showinfo("info2", "estas en descifrado")
@@ -131,7 +124,7 @@ def AES(proceso, modoOperacion, ruta, llave, C0):
             descifradoCFB(ruta, llave, C0)
         if modoOperacion == "OFB":
             descifradoOFB(ruta, llave, C0)
-    buttonRegresarInicio = tk.Button(root, text="Volver al inicio", command=inicio)
+    buttonRegresarInicio = tk.Button(root, text="Volver al inicio", command=inicio, font=("Arial", 14))
     buttonRegresarInicio.pack(side="bottom")
 
 def enviar():
@@ -140,37 +133,30 @@ def enviar():
     messagebox.showinfo("Elección",  "Has elegido:\n" + proceso + "\n"+ modoOperacion)
     limpiarVentana()
 
-    labelLlave = tk.Label(root, text="Llave: ")
+    labelLlave = tk.Label(root, text="Llave: ", font=("Arial", 14))
     labelLlave.pack()
     entryLlave = tk.Entry(root)
     entryLlave.pack()
     
-    labelC0 = tk.Label(root, text="Vector de inicialización (C0): ")
+    labelC0 = tk.Label(root, text="Vector de inicialización (C0): ", font=("Arial", 14))
     labelC0.pack()
     entryC0 = tk.Entry(root)
     entryC0.pack()
    
-    labelArchivoEntrada = tk.Label(root, text="No se ha seleccionado ningún archivo")
+    labelArchivoEntrada = tk.Label(root, text="No se ha seleccionado ningún archivo", font=("Arial", 14))
     labelArchivoEntrada.pack()
     
     labelImg = tk.Label(root)
     labelImg.pack()
 
-    buttonBuscarArchivo = tk.Button(root, text="Buscar archivo", command=lambda: buscarArchivo(labelArchivoEntrada,labelImg))
+    buttonBuscarArchivo = tk.Button(root, text="Buscar archivo", command=lambda: buscarArchivo(labelArchivoEntrada,labelImg), font=("Arial", 14))
     buttonBuscarArchivo.pack()
     
-    
-    buttonRegresarInicio = tk.Button(root, text="Volver al inicio", command=inicio)
+    buttonRegresarInicio = tk.Button(root, text="Volver al inicio", command=inicio, font=("Arial", 14))
     buttonRegresarInicio.pack(side="bottom")
-    buttonEnviarCampos = tk.Button(root, text="Enviar", command=lambda: AES(proceso, modoOperacion,archivoBMP, entryLlave.get(), entryC0.get()))
+    buttonEnviarCampos = tk.Button(root, text="Enviar", command=lambda: AES(proceso, modoOperacion,archivoBMP, entryLlave.get(), entryC0.get()), font=("Arial", 14))
     buttonEnviarCampos.pack(side="bottom")
 
-
-
 inicio()
-
-
-
-
 
 root.mainloop()
